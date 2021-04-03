4ef4831f74cd4103ed31933ded92ab675ebc659e
ankolaver <47784720+ankolaver@users.noreply.github.com>
2021-04-03 21:59:10 +0800

    added failed threadding

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,68 @@
 use std::io;
 use std::io::prelude::*;
 use std::process::Command;
-
+//use crossbeam_utils::thread;
+use std::time::Duration;
+use std::thread;
 
 fn main() {
+
     let stdin = io::stdin();
-    let mut current_key = String::from("");
 
-    for line in stdin.lock().lines() {
+    //extract packages into an array
+    let packages = stdin.lock().lines();
+    let mut vecc  = Vec::new(); 
 
-        //let line = String::from(line.unwrap());
-        let line = line.expect("failed");
-        println!("====== Package:  {} ====",line);
+    for package in packages {
+        let temp = package.expect("failed");
+        vecc.push(temp);
 
-        let output = Command::new("dnf")
-        .args(&["info",&line])
-        .output()
-        .expect("failed to execute process");
-
-        let output = String::from_utf8_lossy(&output.stdout).to_string();
-        let split_str: Vec<&str> = output.split("\n").collect();
-        //println!("{:?}",split_str);
-        /*
-        let second_str = match split_str.get(1){
-            Some(second_str) => second_str,
-            None => "",
-        };   
-        //println!("{:?}",split_str);
-
-        if split_str[0].trim().is_empty() && !(second_str.is_empty()){
-            println!("{}", second_str);
-            current_key = String::from("");
-        }
-        else {
-            current_key = String::from(split_str[0].trim());
-        }
-        */
-        for meta in split_str {
-            //println!("{}",meta);
-            let meta = String::from(meta);
-            
-            let extract = match meta.get(..11) {
-                Some(meta) => meta,
-                None => "",
-            };
-            if extract == "Description"{
-                println!("--{}",meta);    
-            }
-        }
-        
-        
-        //println!("Here's the user input: {}", line);
     }
+    
+    
+    for line in vecc {
+        let child = thread::spawn(move || {
+        //s.spawn(move |_| {
+            //extract the names of pacakges to search for
+            //let nline = line.expect("failed");
+            let seline: Vec<&str> = line.split(" ").collect();
+            
+            //get name of package
+            let package_name = seline[0];
+            
+            get_pkg_info(package_name)
+        
+        });
+        child.join().unwrap();
+            
+    };
+        
+   
+    
     println!("Exiting Application!");
+}
+
+fn get_pkg_info(package_name:&str) {
+    //execute dnf info command
+    let output = Command::new("dnf")
+    .args(&["info",package_name])
+    .output()
+    .expect("failed to execute process");
+
+    let output = String::from_utf8_lossy(&output.stdout).to_string();
+    let split_str: Vec<&str> = output.split("\n").collect();
+
+    for meta in split_str {
+        let meta = String::from(meta);
+        
+        let extract = match meta.get(..11) {
+            Some(meta) => meta,
+            None => "",
+        };
+        if extract == "Description"{
+            println!("====== Package: {} ====",package_name);
+            println!("--{}\n",meta);
+            break;    
+        }
+    }
 }
