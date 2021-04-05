14025460899bd17d5269051a54a5d56025ff69dd
ankolaver <47784720+ankolaver@users.noreply.github.com>
2021-04-05 17:39:20 +0800

    added distro specific cmds

--- src/pkg_info.rs
+++ src/pkg_info.rs
@@ -1,21 +1,64 @@
 use std::process::Command;
+use colored::*;
 
 pub fn get_pkg_info(line: String) -> String {
 
-    //extract the names of pacakges to search for
-    //let nline = line.expect("failed");
-    let seline: Vec<&str> = line.split(" ").collect();
-    
-    //get name of package
-    let package_name = seline[0];
+    //Check Linux Distro Version
+    let version = Command::new("cat")
+        .args(&["/proc/version"])
+        .output()
+        .expect("failed to find Linux Version");
+
+    let distro = String::from_utf8_lossy(&version.stdout).to_string();
 
     //execute dnf info command
-    let output = Command::new("dnf")
-    .args(&["info",package_name])
-    .output()
-    .expect("failed to execute process");
+    let output = if distro.contains("Red Hat") == true { 
+        //extract the names of pacakges to search for
+        let package_data: Vec<&str> = line.split(" ").collect();
+        
+        //get name of package
+        let package_name = package_data[0];
 
-    let output = String::from_utf8_lossy(&output.stdout).to_string();
+        let exec = Command::new("dnf")
+            .args(&["info",package_name])
+            .output()
+            .expect("failed to execute process");
+
+        (exec, package_name)
+
+    } else if distro.contains("ubuntu") == true {
+        //extract the names of pacakges to search for
+        let package_data: Vec<&str> = line.split("/").collect();
+        
+        //get name of package
+        let package_name = package_data[0];
+        
+        let exec = Command::new("apt")
+            .args(&["info",package_name])
+            .output()
+            .expect("failed to execute process");
+        
+        (exec, package_name)
+        
+    } else {
+
+        //extract the names of pacakges to search for
+        let package_data: Vec<&str> = line.split("/").collect();
+        
+        //get name of package
+        let package_name = package_data[0];
+
+        let exec = Command::new("yay")
+            .args(&["info",package_name])
+            .output()
+            .expect("failed to execute process");
+
+        (exec, package_name)
+    };
+
+    let (execution_str, package_name) = output;
+    
+    let output = String::from_utf8_lossy(&execution_str.stdout).to_string();
     let split_str: Vec<&str> = output.split("\n").collect();
 
     for meta in split_str {
@@ -26,8 +69,8 @@ pub fn get_pkg_info(line: String) -> String {
             None => "",
         };
         if extract == "Description"{
-            println!("====== Package: {} ======",package_name);
-            println!("--{}\n",meta);
+            println!("====== Package: {} ======",package_name.color("blue"));
+            println!("--{}",meta);
 
             //extract updates from rust
             let get_last_update = Command::new("rpm")
@@ -44,7 +87,7 @@ pub fn get_pkg_info(line: String) -> String {
                 None => "",
             };
 
-            println!("--Last Update: {}",extract_2.trim());
+            println!("--My Last Update: {}\n",extract_2.trim());
             break;    
         }
     }
